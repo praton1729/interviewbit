0b900ca773d3be75f5520bceae53e4686719aca9
praton <praton@pratonian>
2018-09-01 00:53:00 +0530

    bug fixed for corner cases

--- Linked_List/remove_nth_element_from_end.c
+++ Linked_List/remove_nth_element_from_end.c
@@ -1,29 +1,25 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-// Definition for singly-linked list.
-
-struct ListNode {
-     int val;
-     struct ListNode *next;
- };
- 
- typedef struct ListNode listnode;
- 
- listnode* listnode_new(int val) {
-     listnode* node = (listnode *) malloc(sizeof(listnode));
-     node->val = val;
-     node->next = NULL;
-     return node;
- }
-
-
-// @input A : Head pointer of linked list 
-// @input B : Integer
-// 
-// @Output head pointer of list.
-
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ * 
+ * typedef struct ListNode listnode;
+ * 
+ * listnode* listnode_new(int val) {
+ *     listnode* node = (listnode *) malloc(sizeof(listnode));
+ *     node->val = val;
+ *     node->next = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Head pointer of linked list 
+ * @input B : Integer
+ * 
+ * @Output head pointer of list.
+ */
 listnode* removeNthFromEnd(listnode* A, int B) {
     
     int len = 0;
@@ -36,24 +32,41 @@ listnode* removeNthFromEnd(listnode* A, int B) {
         temp = temp->next;
     }
     
-    printf("%d",len);
+    //printf("%d",len);
     
     temp = A;
+    int i = 0;
     
-    if(len > B)
+    if(len==0)
     {
-        A = A->next;
+        return NULL;
     }
-    else
+    
+    if(len==1 && B==1)
     {
-        for(int i=0;i<len-B;i++)
+        return NULL;
+    }
+    
+    if(len < B)
+    {
+        temp = A->next;
+        return temp;
+    }
+    else if(len > B)
+    {
+        for(i=1;i<len-B;i++)
         {
             temp = temp->next;        
         }
         
-        temp = (temp->next)->next;
+        temp->next = (temp->next)->next;
+        return A;
+    }
+    else
+    {
+        return temp->next;
     }
     
-    return A;
+    return temp;
 }
 
