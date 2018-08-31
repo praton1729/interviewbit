d469a3d9d6fdc93b010a9ff4b19299f641943d35
praton <praton@pratonian>
2018-09-01 00:52:22 +0530

    bug fixed for row copying

--- Arrays/set_matrix_zeros.py
+++ Arrays/set_matrix_zeros.py
@@ -2,7 +2,7 @@
 
 def setZeroes(A):
 
-    B = A
+    B = [row[:] for row in A]
 
     rows = len(A)
 
@@ -24,12 +24,20 @@ def setZeroes(A):
         for j in range(0,cols):
 
             if(A[i][j]==0):
+#                print "B is"
+#                print B
+#                print "\n"
+#                print "A is "
+#                print A
+#                print "\n"
+#
                 set_row(i,j)
                 set_column(i,j)
 
     return B
 
 
-A = [[1,0,1],[1,1,1],[1,1,1]]
+#A = [[1,0,1],[1,1,1],[1,1,1]]
+A = [[0, 0],[1, 0]]
 
 print(setZeroes(A))
